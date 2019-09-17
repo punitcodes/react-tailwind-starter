9e9b8a37a5d81da082b2ee797e0b77cf77345e90
punit2502 <punit.makwana25@gmail.com>
2019-09-17 19:11:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ReactJS + Tailwind CSS (with purgecss)
+
+React version: 16.9.0
+TailwindCSS version: 1.1.2
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-gray-200 h-screen">
+      <h1 className="text-center text-gray-900 text-2xl font-bold p-4">
+        Hello, World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
